d7a1e9d5ef380af1682b2b223973e8dec871c195
intheclouds <patrickmurr93@gmail.com>
2023-05-13 12:59:39 -0400

    Moved HVR and world prefab builder to Packages

---
+++ Assets/Packages/HurricaneVR/Framework/Scripts/Core/Sockets/HVRSocketHoverAction.cs
@@ -0,0 +1,25 @@
+﻿using HurricaneVR.Framework.Core.Grabbers;
+using UnityEngine;
+
+namespace HurricaneVR.Framework.Core.Sockets
+{
+    public abstract class HVRSocketHoverAction : MonoBehaviour
+    {
+        public Transform Target;
+        public Coroutine _hoverRoutine { get; protected set; }
+
+        protected virtual void Start()
+        {
+
+        }
+
+        protected virtual void Update()
+        {
+
+        }
+
+        public abstract void OnHoverEnter(HVRSocket socket, HVRGrabbable grabbable, bool isValid);
+
+        public abstract void OnHoverExit(HVRSocket socket, HVRGrabbable grabbable, bool isValid);
+    }
+}
